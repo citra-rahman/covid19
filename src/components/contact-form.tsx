203c91bb4ffb1f844536382b64c97f86ae83e556
citra-rahman <citrapuspita.r@gmail.com>
2024-08-24 19:38:42 +0700

    change react-hook-form to formik

--- src/components/contact-form.tsx
+++ src/components/contact-form.tsx
@@ -1,44 +1,50 @@
 "use client";
 import { useState } from "react";
-import { useForm, SubmitHandler } from "react-hook-form";
 import api from "@/config/base-url";
-import { User } from "@/lib/types";
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
+
 
 export default function ContactForm() {
   const [success, setSuccess] = useState(false);
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<User>();
 
-  const onSubmit: SubmitHandler<User> = (data) => {
-    try {
-      api.post('/api/email', data)
-        .then(() => {
-          setSuccess(true);
-          window.location.href = "/"
-        });
-    } catch (error: any) {
-      console.error(error);
-    }
-  };
+  const emailSchema = Yup.object().shape({
+    email: Yup.string().email('Invalid email').required('Required'),
+  });
   return (
     <>
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="flex flex-row flex-no-wrap w-[30vw] px-[2vw] rounded-full bg-white justify-between">
-        <input
-          type="email"
-          placeholder="your-email@mail.com"
-          {...register("email", { required: true })}
-          className="text-center text-[1vw] w-[17vw] h-[5vh] my-[2vh]" />
-        <button
-          type="submit"
-          className="text-[1vw] text-white bg-red-400  w-[8vw] px-[0.5vw] my-[1.5vh] rounded-full">
-          Send
-        </button>
-      </form>
+      <Formik
+        initialValues={{
+          email: '',
+        }}
+        validationSchema={emailSchema}
+        onSubmit={data => {
+          try {
+            api.post('/api/email', data)
+              .then(() => {
+                setSuccess(true);
+              });
+          } catch (error: any) {
+            console.error(error);
+          }
+        }}
+      >
+        {({ errors, touched }) => (
+          <Form className="flex flex-row flex-no-wrap w-[30vw] px-[2vw] rounded-full bg-white justify-between">
+            <Field
+              name="email"
+              type="email"
+              placeholder="your-email@mail.com"
+              className="text-center text-[1vw] w-[17vw] h-[5vh] my-[2vh]" />
+            {errors.email && touched.email ? <div>{errors.email}</div> : null}
+            <button
+              type="submit"
+              className="text-[1vw] text-white bg-red-400  w-[8vw] px-[0.5vw] my-[1.5vh] rounded-full">
+              Send
+            </button>
+          </Form>
+        )}
+      </Formik>
       {
         success &&
         <span className="text-red-400 text-center px-[6vw] py-[2vh]">We will contact you soon!</span>
